38ea68f4449fc139879617669a8d88bf42e2f23f
Ulrich Francomme <ulrich.francomme@gmail.com>
2024-02-08 19:55:24 +0400

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+bootstrapApplication(AppComponent, {
+  providers: []
+})
+  .catch(err => console.error(err));
